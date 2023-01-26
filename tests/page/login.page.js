4b7469e0d25abef1465592b82a163fb952ac32a2
litaamelia <litaamelia126@gmail.com>
2023-01-26 21:37:39 +0700

    Error BASE_URL

---
+++ tests/page/login.page.js
@@ -0,0 +1,4 @@
+export const usernameField = '#user-name';           //id
+export const passwordField = '[name="password"]'    //name
+export const loginButton = ".submit-button"         //class
+export const errorAlert = '[data-test="error"]'     //data-test
